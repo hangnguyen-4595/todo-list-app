93fbe479f1586f1772f7da7787352d3fad44e81a
hangnguyen-4595 <nthang59205@gmail.com>
2025-10-21 22:13:34 +0700

    Thêm hàm list_tasks- để liệt kê tất cả công việc

--- app.py
+++ app.py
@@ -9,3 +9,21 @@ if __name__ == "__main__":
     print("Chào mừng đến với ứng dụng To-Do List!")
     add_task("Học bài Git và GitHub")
     add_task("Làm bài tập thực hành ở nhà")
+
+##BTVN
+#Liet ke tat ca cong viec
+tasks = []
+def add_task(name):
+    tasks.append(name)
+    print(f"Đã thêm công việc: {name}")
+def list_tasks():
+    if not tasks:
+        print("Không có công việc nào.")
+    else:
+        print("Danh sách công việc:")
+        for i, task in enumerate(tasks, start=1):
+            print(f"{i}. {task}")
+if __name__ == "__main__":
+    add_task("Học bài Git")
+    add_task("Làm bài tập")
+    list_tasks()
